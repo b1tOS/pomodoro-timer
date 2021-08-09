a4b26fc2a11bfef6c35c74b71efcf5f2af2dcaa6
b1t <b1toffsec@gmail.com>
2021-08-10 02:55:24 +0300

    commitik

--- pomodoro.py
+++ pomodoro.py
@@ -12,16 +12,13 @@ while True:
     mins = secsForWork//60
     print('\t' + f'{mins:>02}:{secsForWork-mins*60:>02}', end='\r')
     time.sleep(1)
-
     if secsForWork == 0:
         playsound('http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3')
-
         while timeout != 0:
             timeout -= 1
             mins = timeout//60
             print('\t' + f'{mins:>02}:{timeout-mins*60:>02}', end='\r')
             time.sleep(1)
-            
         playsound('http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3')
         k += 1
         timeout = 900 if k % 4 == 0 else 300
